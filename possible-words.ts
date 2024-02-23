c2f8eb8c5e435102be29d0d8aa264bd8497b10d5
Will Floyd <floyd.will@gmail.com>
2024-02-23 14:03:41 -0800

    add jsdocs

--- possible-words.ts
+++ possible-words.ts
@@ -1,3 +1,10 @@
+/**
+ * Swaps the characters of a given string at the given indices
+ * @param {string} str - the input string
+ * @param {number} i - the first index
+ * @param {number} j - the second index
+ * @returns {string}
+ */
 export const swapCharsAtIndices = (str: string, i: number, j: number): string => {
   if (str.length < 2) return str
   if (i === j) return str
@@ -10,6 +17,11 @@ export const swapCharsAtIndices = (str: string, i: number, j: number): string =>
   return split.join('')
 }
 
+/**
+ * Generates a set of the unique substrings of a given string
+ * @param {string} str - the input string
+ * @returns {Set.<string>}
+ */
 export const everySubstring = (str: string): Set<string> => {
   const substrings: Set<string> = new Set()
 
@@ -22,6 +34,13 @@ export const everySubstring = (str: string): Set<string> => {
   return substrings
 }
 
+/**
+ * Generates a set of unique permutations of an input string and its substrings
+ * @param {string} str - the input string to permute
+ * @param {number} [currentIndex=0] - the starting index for the recursive swapping algorithm
+ * @param {Set.<string>} [permutations=new Set()] - the set of permutations that will be returned
+ * @returns {Set.<string>}
+ */
 export const permuteStringAndSubstrings = (str: string, currentIndex: number = 0, permutations: Set<string> = new Set()): Set<string> => {
   if (currentIndex === str.length - 1) {
     permutations.add(str)
@@ -37,6 +56,13 @@ export const permuteStringAndSubstrings = (str: string, currentIndex: number = 0
   return permutations
 }
 
+/**
+ * Generates a list of words in the given wordlist that can be constructed
+ * from permutations of a given string and its substrings.
+ * @param {string} str - the input string to permute
+ * @param {string[]} wordList - the list of words to check permutations of the string against
+ * @returns {string[]} 
+ */
 export const possibleWords = (str: string, wordList: string[]): string[] => {
   const permutations: string[] = [...permuteStringAndSubstrings(str)]
   return permutations.filter((candidate) => wordList.includes(candidate))
